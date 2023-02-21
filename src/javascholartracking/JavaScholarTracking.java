1da3f602fc875c270b9ac536e7156e4a019c9307
HariellSalmeron <hariellsalmeron@gmail.com>
2023-02-21 20:30:06 +0800

    first commit

--- src/javascholartracking/JavaScholarTracking.java
+++ src/javascholartracking/JavaScholarTracking.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gitsample1;
+package javascholartracking;
 
 /**
  *
  * @author Hariell
  */
-public class GitSample1 {
+public class JavaScholarTracking {
 
     /**
      * @param args the command line arguments
